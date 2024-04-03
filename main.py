01b9d1c71c2046902cf84fcb59f8d73fb935e6b6
Sarthak Dhiman <93830724+sarthak-dhiman@users.noreply.github.com>
2024-04-03 18:42:36 +0530

    main code

--- main.py
+++ main.py
@@ -4,50 +4,91 @@
 import board
 import digitalio
 import time
+import busio
+import character_lcd
+lcd_columns = 20
+lcd_rows = 4
+# Initialize I2C bus
+i2c = busio.I2C(board.GP0, board.GP1)  # Change GP0 and GP1 to your SDA and SCL pins
+
+# Initialize LCD class
+lcd = character_lcd.Character_LCD_I2C(i2c, lcd_columns, lcd_rows)
+
+# Clear the LCD screen
+lcd.clear()
+lcd.move_to(0, 1)
+lcd.message = "Welcome to ButtonMorse"
+lcd.clear()
+lcd.move_to(0, 1)
+lcd.message ='Enter Morse'
+Enter = digitalio.DigitalInOut(board.GP16)
+
+Dot = digitalio.DigitalInOut(board.GP17)
+
+Dash = digitalio.DigitalInOut(board.GP18)
 #dot= 0, dash=1
-a = (0,1)
-b = (1,0,0,0)
-c = (1,0,1,0)
-d = (1,0,0)
-e = (0)
-f = (0,0,1,0)
-g = (1,1,0)
-h = (0,0,0,0)
-i = (0,0)
-j = (0,1,1,1)
-k = (1,0,1)
-l = (0,1,0,0)
-m = (1,1)
-n = (1,0)
-o = (1,1,1)
-p = (0,1,1,0)
-q = (1,1,0,1)
-r = (0,1,0)
-s = (0,0,0)
-t = (1)
-u = (0,0,1)
-v = (0,0,0,1)
-w = (0,1,1)
-x = (1,0,0,1)
-y = (1,0,1,1)
-z = (1,1,0,0)
-one = (0,1,1,1,1)
-two = (0,0,1,1,1)
-three =(0,0,0,1,1)
-four = (0,0,0,0,1)
-five = (0,0,0,0,0)
-six =  (1,0,0,0,0)
-seven = (1,1,0,0,0)
-eight = (1,1,1,0,0)
-nine = (1,1,1,1,0)
-ten = (1,1,1,1,1)
-question = (0,0,1,1,0,0)
-exclamation = (1,0,1,0,1,1)
-dot = (0,1,0,1,0,1)
-semicolon = (1,1,0,0,1,1)
-colon = (1,1,1,0,0,0)
-plus = (0,1,0,1,0)
-minus =(1,0,0,0,1)
-slash = (1,0,0,1,0)
-equal = (1,0,0,0,1)
-# morse dictionary completed and represented in form of tuples 
+morse_dictonary = {
+'a' : (0,1),
+'b' : (1,0,0,0),
+'c' : (1,0,1,0),
+'d' : (1,0,0),
+'e' : (0),
+'f' : (0,0,1,0),
+'g' : (1,1,0),
+'h' : (0,0,0,0),
+'i' : (0,0),
+'j' : (0,1,1,1),
+'k' : (1,0,1),
+'l' : (0,1,0,0),
+'m' : (1,1),
+'n' : (1,0),
+'o' : (1,1,1),
+'p' : (0,1,1,0),
+'q' : (1,1,0,1),
+'r' : (0,1,0),
+'s' : (0,0,0),
+'t' : (1),
+'u' : (0,0,1),
+'v' : (0,0,0,1),
+'w' : (0,1,1),
+'x' : (1,0,0,1),
+'y' : (1,0,1,1),
+'z' : (1,1,0,0),
+'1' : (0,1,1,1,1),
+'2' : (0,0,1,1,1),
+'3' : (0,0,0,1,1),
+'4' : (0,0,0,0,1),
+'5' : (0,0,0,0,0),
+'6' :  (1,0,0,0,0),
+'7' : (1,1,0,0,0),
+'8' : (1,1,1,0,0),
+'9' : (1,1,1,1,0),
+'0' : (1,1,1,1,1),
+'?' : (0,0,1,1,0,0),
+'1' : (1,0,1,0,1,1),
+'.' : (0,1,0,1,0,1),
+';' : (1,1,0,0,1,1),
+':' : (1,1,1,0,0,0),
+'+' : (0,1,0,1,0),
+'-' : (1,0,0,0,1),
+'/' : (1,0,0,1,0),
+'=' : (1,0,0,0,1) }
+# morse dictionary completed and represented in form of tuples
+input_value = []
+while not Enter.value:
+    if Dot.value:
+        input_value.append('0')
+    elif Dash.value:
+        input_value.append('1')
+
+translated_text = ''
+for symbol in input_value:
+    for letter, morse_code in morse_dictonary.items():
+        if symbol == ''.join(map(str, morse_code)):
+            translated_text += letter
+            break
+
+lcd.move_to(0, 1)
+
+lcd.message =translated_text
+    
